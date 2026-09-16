48f9260b3f3096b8540d4062b8c85e31724448ef
Billie Hilton <587740+billiegoose@users.noreply.github.com>
2026-09-16 16:08:04 -0400

    Expose all Basys 3 user LEDs to Pypeline

--- include/pypeline/board/basys3/io.py
+++ include/pypeline/board/basys3/io.py
@@ -1,10 +1,25 @@
 # pyright: reportInvalidTypeForm=none
-"""Digilent Basys 3 basic board I/O used by simple Pypeline designs."""
+"""Digilent Basys 3 basic board I/O for Pypeline designs."""
 
 from pypeline import Input, Output, make_clock, uint1_t
 
 # Basys 3 on-board 100 MHz oscillator, PACKAGE_PIN W5.
 clk: Input[uint1_t] = make_clock(100.0)
 
-# First user LED, LD0, PACKAGE_PIN U16.
+# User LEDs LD0..LD15.
 led0: Output[uint1_t]
+led1: Output[uint1_t]
+led2: Output[uint1_t]
+led3: Output[uint1_t]
+led4: Output[uint1_t]
+led5: Output[uint1_t]
+led6: Output[uint1_t]
+led7: Output[uint1_t]
+led8: Output[uint1_t]
+led9: Output[uint1_t]
+led10: Output[uint1_t]
+led11: Output[uint1_t]
+led12: Output[uint1_t]
+led13: Output[uint1_t]
+led14: Output[uint1_t]
+led15: Output[uint1_t]
